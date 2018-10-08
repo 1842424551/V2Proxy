3aac7d76631955471220a1144b480e29a8337f4f
MaruKoh <37258122+MaruKoh@users.noreply.github.com>
2018-10-09 00:38:36 +0800

    Add files via upload

---
+++ upgrade.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+#重装V2Proxy
+rm -rf /usr/local/V2Proxy
+cd /usr/local/
+git clone https://github.com/KiriKira/V2Proxy.git
+cd /usr/local/V2Proxy/
+chmod +x *.py
+
+#重装操作菜单
+rm -rf /usr/local/bin/v2ray
+cp /usr/local/V2Proxy/v2ray /usr/local/bin/
+chmod +x /usr/local/bin/v2ray
+
+#更新v2ray主程序
+bash <(curl -L -s https://install.direct/go.sh)
+
+clear
+echo "脚本已更新！"
