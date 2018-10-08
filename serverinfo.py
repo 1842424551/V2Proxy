ca86840179f12f00eacd95f5ea7ee519854ad482
MaruKoh <37258122+MaruKoh@users.noreply.github.com>
2018-10-09 06:51:00 +0800

    Update serverinfo.py

--- serverinfo.py
+++ serverinfo.py
@@ -39,6 +39,31 @@ print("AlterId  ：%s") % str(readjson.ConfAlterId)
 print("Security ：%s") % str(readjson.ConfSecurity)
 print("Network  ：%s") % str(mystreamnetwork)
 print("%s") % str(mystreamsecurity)
+
+for index, sin_user_conf in enumerate(read_json.multiUserConf):
+    index += 1
+    protocol=sin_user_conf["protocol"]
+    print("%d." % index)
+    print("Group: %s" % sin_user_conf["indexDict"]["group"])
+    print("IP：%s" % sin_user_conf["add"]) 
+    print("Port：%s" % sin_user_conf["port"])
+    if sin_user_conf["email"]:
+        if protocol == "vmess" or protocol == "mtproto" or protocol == "shadowsocks":
+            print("Email: %s" % sin_user_conf["email"])
+        elif protocol == "socks":
+            print("User: %s" % sin_user_conf["email"])
+    if protocol == "vmess":
+        print("UUID：%s" % sin_user_conf["id"])
+        print("Alter ID: %s" % sin_user_conf["aid"])
+        if sin_user_conf["net"] == "h2":
+            print("Network：%s" % ("HTTP/2  伪装Path: " + sin_user_conf["path"])) 
+        elif sin_user_conf["net"] == "ws":
+            print("Network：%s" % ("WebSocket  伪装Host: " + sin_user_conf["host"] + " 伪装Path: " + sin_user_conf["path"])) 
+        else:
+            print("Network：%s" % (sin_user_conf["net"] + " " + sin_user_conf["type"])) 
+        print("TLS: %s" % ("关闭" if sin_user_conf["tls"] == "" else "开启"))
+
+copy_conf = sin_user_conf.copy()
 base64_str = base64.b64encode(bytes(json.dumps(copy_conf), 'utf-8'))
 share_url = "vmess://" + bytes.decode(base64_str) 
 #绿色字体显示
